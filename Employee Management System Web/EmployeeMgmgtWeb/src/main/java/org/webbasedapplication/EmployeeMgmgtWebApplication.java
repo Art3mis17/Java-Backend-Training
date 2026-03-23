0d0ba74103148a1048d1cc30b3e99a277d718720
B A Saran <86791121+Art3mis17@users.noreply.github.com>
2026-03-23 09:50:45 +0530

    Add files via upload

---
+++ Employee Management System Web/EmployeeMgmgtWeb/src/main/java/org/webbasedapplication/EmployeeMgmgtWebApplication.java
@@ -0,0 +1,13 @@
+package org.webbasedapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMgmgtWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeMgmgtWebApplication.class, args);
+	}
+
+}
